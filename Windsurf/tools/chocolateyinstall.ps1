70bdfd591f50def940b23d71c637464a9ff891ce
publicfacingusername <github@mail4j.com>
2025-04-15 23:44:48 -0400

    Update hash

--- Windsurf/tools/chocolateyinstall.ps1
+++ Windsurf/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'exe'
   url           = $url
   softwareName  = 'Windsurf*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '197C096DDAD61607EF0EE9B289147C0C'
+  checksum      = '7CB5A20E5D8C5F2277A3DC8AE9A8AC793A2713B9F879EFD2F3D80CABDB30BDC1'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   silentArgs    = "/VERYSILENT" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
